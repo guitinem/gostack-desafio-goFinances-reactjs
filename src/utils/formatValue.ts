86678f0948dce36549ec2b4bdaa45bf05a93e188
Guilherme Tinem <guizstinem@gmail.com>
2020-09-02 22:22:22 -0300

    :sparkles: Desafio concluido

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const result = Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRl',
+  }).format(value);
+
+  return result;
+};
 
 export default formatValue;
